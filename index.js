157dacce1759ef50809d831e59cbaf83831268b6
faisalkhandbc <mailtofaisal2@gmail.com>
2025-11-10 19:24:43 +0530

    Update environment configuration and enhance .gitignore for better project management

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 // Create Application
 const app = express();
@@ -16,6 +17,6 @@ app.use(cors());
 // Use API routes
 app.use('/', require('./routes/api'));
 
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
+app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}/`);
 }); 
