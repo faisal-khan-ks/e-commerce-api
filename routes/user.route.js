77a9da1ac8d4c67050c1fb8061c4a7eb91eb516d
faisalkhandbc <mailtofaisal2@gmail.com>
2026-05-12 12:58:15 +0530

    validation removed

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const UserCtrl = require('../controllers/user.controller');
-const { validateAddUser } = require('../validators/user.validator');
 
 const router = express.Router();
 
-router.get('/', validateAddUser, UserCtrl.getAllUsers);
+router.get('/', UserCtrl.getAllUsers);
 
 router.post('/', UserCtrl.addUser);
 
